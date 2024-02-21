c9421899c128fe8645efc6dd1daaa6455f18b56e
GIL32610 <gil.00000032610@unicap.br>
2024-02-21 14:10:07 -0300

    Java Record

---
+++ src/RecordProject/LPAStudent.java
@@ -0,0 +1,7 @@
+package RecordProject;
+
+public record LPAStudent(String id, String name, String dateOfBirth, String classList) {
+}
+//Immutable members!
+//private and final
+//component field
--- src/RecordProject/Main.java
+++ src/RecordProject/Main.java
@@ -3,18 +3,29 @@ package RecordProject;
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i < 5; i++) {
-            Student s = new Student("S572432" + i,
-                    switch (i) {
-                        case 1 -> "red";
-                        case 2 -> "carolina";
-                        case 3 -> "eduarda";
-                        case 4 -> "cecilia";
-                        case 5 -> "helena";
-                        default -> "dunno";
-                    },
-                    "21/02/2000",
-                    "Library");
+            LPAStudent s = new LPAStudent("S572432" + i, switch (i) {
+                case 1 -> "red";
+                case 2 -> "carolina";
+                case 3 -> "eduarda";
+                case 4 -> "cecilia";
+                case 5 -> "helena";
+                default -> "dunno";
+            }, "21/02/2000", "Library");
             System.out.println(s);
         }
+
+        Student pojoStudent = new Student("S42341", "Ann",
+                "06/07/98", "JavaMasterClass");
+        LPAStudent recordStudent = new LPAStudent("S514134", "Bill",
+                "12/04/2001", "JavaMasterClass");
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+        //recordStudent.setClassList();
+
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
+
     }
 }
