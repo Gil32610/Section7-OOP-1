97a91c8d200820c62b64975b7d3c3e116bc7843f
GIL32610 <gil.00000032610@unicap.br>
2024-02-21 14:47:47 -0300

    Coding Exercise 35

---
+++ src/CodingExercisesSection7/CodingExercise35.java
@@ -0,0 +1,38 @@
+package CodingExercisesSection7;
+
+ class ComplexNumber {
+     private double real;
+     private double imaginary;
+
+     public ComplexNumber(double real, double imaginary) {
+         this.real = real;
+         this.imaginary = imaginary;
+     }
+
+     public double getReal() {
+         return real;
+     }
+
+     public double getImaginary() {
+         return imaginary;
+     }
+
+     public void add (double real, double imaginary){
+         this.real+= real;
+         this.imaginary+= imaginary;
+     }
+
+     public void add (ComplexNumber complexNumber){
+         this.add(complexNumber.getReal(),complexNumber.getImaginary());
+     }
+
+     public void subtract(double real, double imaginary){
+         this.real-= real;
+         this.imaginary-= imaginary;
+     }
+
+     public void subtract (ComplexNumber complexNumber){
+         this.subtract(complexNumber.getReal(),complexNumber.getImaginary());
+     }
+
+ }
