f55d1b4e98c35600db2cb2390fc44b3a49c91b1a
GIL32610 <gil.00000032610@unicap.br>
2024-02-21 15:15:53 -0300

    Dog child class

--- src/Inheritance/Dog.java
+++ src/Inheritance/Dog.java
@@ -1,7 +1,30 @@
 package Inheritance;
 
 public class Dog extends Animal {
-    public Dog(){
+
+    private String earShape;
+
+    private String tailShape;
+
+
+    public Dog() {
         super("Mutt", "Big", 50);
+    }
+    public Dog(String type, double weight){
+        this(type, weight, "Perky", "Curled");
+    }
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
     }
 }
